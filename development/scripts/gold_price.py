679410bb0d09eb77ce9b2ccdf657eed0d96f7af4
sumitborkar05 <sumitborkar05@gmail.com>
2026-02-28 16:54:41 +0530

    restructure repo

--- development/scripts/gold_price.py
+++ development/scripts/gold_price.py
@@ -4,7 +4,24 @@ import requests
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+import os
+from pathlib import Path
+
 from IPython.display import display
+
+
+try:
+    # Works in .py script
+    BASE_DIR = Path(__file__).resolve().parents[2]
+except NameError:
+    # Works in Jupyter Notebook
+    BASE_DIR = Path().resolve().parents[1]
+
+DATA_DIR = BASE_DIR / "data"
+DATA_DIR.mkdir(exist_ok=True)
+
+print("Base directory:", BASE_DIR)
+print("Data directory:", DATA_DIR)
 
 
 def download_etf_data(ticker_symbol: str, period: str = "5y", interval: str = "1d") -> pd.DataFrame:
@@ -25,7 +42,7 @@ def download_etf_data(ticker_symbol: str, period: str = "5y", interval: str = "1
     return df
 
 
-def save_dataframe_to_csv(df: pd.DataFrame, filename: str) -> None:
+def save_dataframe_to_csv(df: pd.DataFrame, DATA_DIR: str, filename: str, subfolder="process"):
     """
     Saves a pandas DataFrame to a CSV file.
 
@@ -33,15 +50,18 @@ def save_dataframe_to_csv(df: pd.DataFrame, filename: str) -> None:
         df (pd.DataFrame): The DataFrame to save.
         filename (str): The name of the CSV file.
     """
-    print(f"Saving data to {filename}...")
-    df.to_csv(filename, index=False)
-    print("Data saved successfully.")
-    return 
+    save_dir = DATA_DIR / subfolder
+    save_dir.mkdir(parents=True, exist_ok=True)
+
+    file_path = save_dir / filename
+    df.to_csv(file_path, index=False)
+    print(f"Data saved successfully to: {file_path}")
+    return
 
 ticker = "HDFCGOLD.NS"
 file_name = "hdfc_gold_5_year_etf.csv"
 hdfc_gold_5_year_etf = download_etf_data(ticker_symbol=ticker, period="5y", interval="1d")
-save_dataframe_to_csv(df=hdfc_gold_5_year_etf, filename=file_name)
+save_dataframe_to_csv(df=hdfc_gold_5_year_etf, DATA_DIR=DATA_DIR, filename=file_name)
 
 
 def get_scheme_code_list (scheme_name: str) -> pd.DataFrame:
@@ -58,21 +78,30 @@ df_scheme = get_scheme_code_list(scheme_name='hdfc gold')
 display(df_scheme)
 
 
-def get_nav(scheme_code: str) -> pd.DataFrame:
+def get_nav(scheme_code: str, DATA_DIR: str, subfolder="process") -> pd.DataFrame:
 
   res = requests.get(f"https://api.mfapi.in/mf/{scheme_code}")
   data = res.json()
   df_nav = pd.DataFrame(data['data'])
-  df_nav.to_csv(f"{scheme_code}.csv", index=False)
+
+  save_dir = DATA_DIR / subfolder
+  save_dir.mkdir(parents=True, exist_ok=True)
+
+  filename = f"{scheme_code}.csv"
+
+  file_path = save_dir / filename
+  print(save_dir)
+  
+  df_nav.to_csv(file_path, index=False)
   return df_nav
 
 
-df_nav = get_nav(scheme_code='119132')
+df_nav = get_nav(scheme_code='119132', DATA_DIR=DATA_DIR)
 
 
-def compare_mf_nav_with(mf_csv, nav_csv) -> pd.DataFrame:
-  df_mf = pd.read_csv(mf_csv)
-  df_nav = pd.read_csv(nav_csv)
+def compare_mf_nav_with(mf_csv, nav_csv, DATA_DIR: str, subfolder="process") -> pd.DataFrame:
+  df_mf = pd.read_csv(DATA_DIR/ subfolder/ mf_csv)
+  df_nav = pd.read_csv(DATA_DIR/ subfolder/ nav_csv)
 
   df_nav = df_nav.rename(columns={'date': 'Date'})
   df_nav['Date'] = pd.to_datetime(df_nav['Date'])
@@ -92,7 +121,7 @@ def compare_mf_nav_with(mf_csv, nav_csv) -> pd.DataFrame:
   fig, ax1 = plt.subplots(figsize=(10, 5))
 
   # Plot Close Price on primary y-axis with a solid blue line
-  sns.lineplot(data=combine_df, x='Date', y='Close', label='Close Price', color='blue', linestyle='-', ax=ax1)
+  sns.lineplot(data=combine_df, x='Date', y='Close', label='ETF Close Price', color='blue', linestyle='-', ax=ax1)
   ax1.set_xlabel('Date')
   ax1.set_ylabel('Close Price', color='blue')
   ax1.tick_params(axis='y', labelcolor='blue')
@@ -113,4 +142,4 @@ def compare_mf_nav_with(mf_csv, nav_csv) -> pd.DataFrame:
 
   return combine_df
 
-combine_df = compare_mf_nav_with(mf_csv='hdfc_gold_5_year_etf.csv', nav_csv='119132.csv')
+combine_df = compare_mf_nav_with(mf_csv='hdfc_gold_5_year_etf.csv', nav_csv='119132.csv', DATA_DIR=DATA_DIR)
